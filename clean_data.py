2b13e316dd9eeaad07f3b85c1601ccca0f283d71
mandab749 <54584390+mandab749@users.noreply.github.com>
2020-04-21 09:38:57 -0400

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -47,7 +47,6 @@ def remove_paragraph_tags(line):
     fixupcontent = line[4]
     clean = re.compile(r'<.*?>')
     fixupcontent1 = re.sub(clean, '', fixupcontent)
-    fixupcontent1 = str(fixupcontent1)
     emoji_pattern = re.compile("["
     u"\U0001F600-\U0001F64F" # emoticons
     u"\U0001F300-\U0001F5FF"  # symbols & pictographs
